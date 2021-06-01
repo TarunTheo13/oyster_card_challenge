83d1f59a12fbdc474c01f78e2a5405dd68f10778
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-06-01 16:13:48 +0100

    Closing #5: added test for top_up method and accompanying code

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,10 @@ describe Oystercard do
   it 'has a starting balance of zero' do
     expect(subject.balance).to eq 0
   end
+
+  it 'allows us to add money' do
+    subject.top_up(10)
+    subject.top_up(7)
+    expect(subject.balance).to eq 17
+  end
 end
