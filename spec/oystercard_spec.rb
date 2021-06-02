fa6739f68635f176458b13b2217e48d055fd3ed8
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-06-02 14:50:21 +0100

    Closing #7: added method to deduct money

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -17,4 +17,10 @@ describe Oystercard do
     expect{ subject.top_up 1 }.to raise_error 'Maximum balance exceeded'
   end
 
+  it { is_expected.to respond_to(:deduct).with(1).argument }
+
+  it 'deducts an amount from the balance' do
+    subject.top_up(20)
+    expect{ subject.deduct 3}.to change{ subject.balance }.by -3
+  end
 end
